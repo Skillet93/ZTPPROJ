babb87e0805841b1709330a9ba190e85924a897c
Skillet93 <rsmreczak8@gmail.com>
2017-10-30 21:26:17 +0100

    Replaced the wrong project

---
+++ PROJ2/CarPresentation/CarPresentation/Data/DatabaseInitializer.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Driver;
+
+namespace CarPresentation.Data
+{
+    public class DatabaseInitializer
+    {
+        public static IMongoDatabase Initialize()
+        {
+            var client = new MongoClient(Constants.Db.DatabaseUrl);
+            return client.GetDatabase(Constants.Db.DatabaseName);
+        }
+    }
+}
---
+++ PROJ2/CarPresentation/CarPresentation/Repository/ICarRepository.cs
@@ -0,0 +1,9 @@
+﻿using CarPresentation.Models;
+
+namespace CarPresentation.Repository
+{
+    public interface ICarRepository
+    {
+        CarForm FindCarDetails(string model,  string langCode);
+    }
+}
